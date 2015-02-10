be36c66f2b6811563220d45b830e568088c5331d
akrigline <andrew@akrigline.com>
2015-02-10 13:00:43 -0500

    HELP
    I NEED SOMEBODY

--- UAkronDesign v2/js/main.js
+++ UAkronDesign v2/js/main.js
@@ -1,1 +1,15 @@
+if (document.getElementById('info')){
+	$(document).ready( function(){
+		setTimeout( function (){
+	        $('.rate').html(99);
+	    }, 1000 );
+		setTimeout( function (){
+	        $('.ratio1').html(118);
+	        $('.ratio2').html(1);
+	    }, 1100 );
+	    setTimeout( function (){
+	        $('.size').html(200);
+	    }, 1200 );
+	});
+}
 
